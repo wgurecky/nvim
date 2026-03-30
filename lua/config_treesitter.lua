62599e43dd7f44c883acdd5d739d6b1cfffc3a5f
William Gurecky <william.gurecky@gmail.com>
2026-03-30 13:51:11 -0400

    use treesitter defaults

--- lua/config_treesitter.lua
+++ lua/config_treesitter.lua
@@ -1,14 +1,13 @@
 -- treesitter
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "cpp", "rust", "python", "markdown", "rst", "lua", "vim", "json" },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = { "fortran", "latex", "markdown" },
-  highlight = {
-    enable = true,
-    disable = { "fortran", "latex", "markdown"},
-    additional_vim_regex_highlighting = false,
-  }
-}
-
+-- require'nvim-treesitter.configs'.setup {
+--   -- A list of parser names, or "all"
+--   ensure_installed = { "c", "cpp", "rust", "python", "markdown", "rst", "lua", "vim", "json" },
+--   sync_install = false,
+--   auto_install = true,
+--   ignore_install = { "fortran", "latex", "markdown" },
+--   highlight = {
+--     enable = true,
+--     disable = { "fortran", "latex", "markdown"},
+--     additional_vim_regex_highlighting = false,
+--   }
+-- }
